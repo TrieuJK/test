6b909931c1fb6de9cb9f4c19a1e99cb331634ca1
Trieu <Trieu@gmail.com>
2025-06-01 10:01:13 +0700

    changed after return 1aad37

--- src/test.cpp
+++ src/test.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    cout << "This project uses the C++17 language standard version." << endl;
+    cout << "This is a simple C++ program." << endl;
     return 0;
 }
