28cc1b0400d0fef437ebfbd2f077c5134beb854a
Trieu <Trieu@gmail.com>
2025-06-01 10:16:42 +0700

    a

--- src/test.cpp
+++ src/test.cpp
@@ -5,5 +5,7 @@ int main() {
     cout << "Hello, World!" << endl;
     cout << "This project uses the C++17 language standard version." << endl;
     cout << "This is a simple C++ program." << endl;
+    cout << "It prints a greeting message to the console." << endl;
+    cout << "Feel free to modify and expand this code." << endl;
     return 0;
 }
